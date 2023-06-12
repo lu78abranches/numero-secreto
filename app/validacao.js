880c74d96eefcea46851d060a6090d4e64040cbd
lu78abranches <luisabranches.surf@hotmail.com>
2023-06-12 12:07:17 -0300

    implementacoes finais no modulo validacao.js

--- app/validacao.js
+++ app/validacao.js
@@ -2,11 +2,30 @@ function validaChute(chute) {
     const numero = +chute;
 
     if (chuteForInvalido(numero)) {
-        console.log('Valor inválido');
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
     }
 
     if (numeroMaiorOuMenorQuePermitido(numero)) {
-        console.log(`Número inválido: O chute precisa estar entre ${menorValor} e ${maiorValor}`)
+        elementoChute.innerHTML += `
+        <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
+        `
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
+        `
     }
 }
 
@@ -17,5 +36,7 @@ function chuteForInvalido(numero) {
 function numeroMaiorOuMenorQuePermitido(numero) {
     return numero > maiorValor || numero < menorValor;
 }
+
+
 
     
