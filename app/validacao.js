596fe14dbd6aaa6cad5ee70456b920b3e37722fd
lu78abranches <luisabranches.surf@hotmail.com>
2023-06-15 01:13:24 -0300

    Adicionando placar de pontuação no jogo

--- app/validacao.js
+++ app/validacao.js
@@ -1,8 +1,12 @@
+let pontos = document.getElementById('pontos')
+pontos.innerHTML = 100;
+
 function validaChute(chute) {
     const numero = +chute;
 
     if (chuteForInvalido(numero)) {
-        if (chute.toUpperCase() === "GAME OVER") {
+        
+        if (chute.toUpperCase() === "GAME OVER" || pontos.innerHTML <= 0) {
             document.body.innerHTML =
                 `
                     <h2>Game Over!!!</h2>
@@ -19,6 +23,7 @@ function validaChute(chute) {
         }
         else {
             elementoChute.innerHTML += '<div>Valor inválido</div>'
+            pontos.innerHTML = parseInt(pontos.innerHTML) + parseInt(-5);
         }
 
 
@@ -29,18 +34,25 @@ function validaChute(chute) {
         elementoChute.innerHTML += `
         <div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>
         `
-        return
+        pontos.innerHTML = parseInt(pontos.innerHTML) + parseInt(-5);
     }
 
     if (numero === numeroSecreto) {
+        
         document.body.innerHTML = `
+            <div class="pontos-container">
+            <h3 class="pontuacao-texto">Pontos</h3>
+            <div class="pontuacao" id="pontos">${pontos.innerHTML = parseInt(pontos.innerHTML) + parseInt(20)}</div>
+            </div>
             <h2>Você acertou!</h2>
             <h3>O número secreto era ${numeroSecreto}</h3>
             <h4 class="jogar-novamente">Fale "Jogar mais uma vez" para continuar jogando</h4>
             <h4 class="terminar-jogo">Terminar o jogo diga "Game Over"</h4>
         `
+        
         confetti.start();
         setTimeout(function () { confetti.stop(); }, 15000);
+        
         
 
 
@@ -49,10 +61,12 @@ function validaChute(chute) {
         elementoChute.innerHTML += `
         <div>O número secreto é menor <i class="fa-solid fa-down-long"></i></div>
         `
+        pontos.innerHTML = parseInt(pontos.innerHTML) + parseInt(-5);
     } else {
         elementoChute.innerHTML += `
         <div>O número secreto é maior <i class="fa-solid fa-up-long"></i></div>
         `
+        pontos.innerHTML = parseInt(pontos.innerHTML) + parseInt(-5);
     }
 }
 
